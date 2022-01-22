37a653e8a2fb625c7bc16900eb6556d1df827484
Dynamic Coding With Amit <89675208+DynamicCodingWithAmit@users.noreply.github.com>
2022-01-22 21:56:03 +0530

    coding file with image

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let inputsearch_v = document.getElementById('inputsearch');
+
+inputsearch_v.addEventListener("click",()=>{
+    let inputbox_v =document.getElementById('inputbox');
+    inputbox_v.style.display="block";
+
+    let email_V = document.getElementById('email');
+    email_V.style.fontSize="15px";
+    email_V.style.transition="2s";
+})
